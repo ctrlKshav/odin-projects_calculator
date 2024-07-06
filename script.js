30640cbf3187d45e63e04fd7cf9d54c2659dc923
ctrlKshav <keshavm9978@gmail.com>
2024-07-06 20:06:33 +0530

    Calculator can now multiple digit calculations but it can't do it in one go yet

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 ﻿function addTwoNumbers(op1,op2){
-    return op1+op2;
+    return Number(op1)+Number(op2);
 }
 function subtractTwoNumbers(op1,op2){
-    return op1-op2;
+    return Number(op1)-Number(op2);
 }
 function multiplyTwoNumbers(op1,op2){
-    return op1*op2;
+    return Number(op1)*Number(op2);
 }
 function divideTwoNumbers(op1,op2){
-    return op1/op2;
+    return Number(op1)/Number(op2);
 }
-function operation(op1,op2,opr){
+function operate(op1,op2,opr){
     if(opr==='+')
         return addTwoNumbers(op1,op2)
     else if(opr==='-')
@@ -26,44 +26,48 @@ function operation(op1,op2,opr){
 display=document.querySelector('.display')
 digitBtns=document.querySelectorAll('.numbers .btn')
 operatorBtns=document.querySelectorAll('.operators .btn')
-let operand1,operand2,operator;
+let operand1='',operand2='',operator;
 let opTemp,ansTemp;
-let flag=false
+let eqFlag=false,opFlag=false
 
 const digitClicked=function(event){
     opTemp=event.target.textContent
-    display.textContent=opTemp
-
-    if(operand1!==undefined)
-        operand2=Number(opTemp)
-    else
-        operand1=Number(opTemp)
+    
+    if(operand1!=='' && opFlag){
+        operand2+=(opTemp)
+        display.textContent=operand2
+    }
+    else{
+        operand1+=(opTemp)
+        display.textContent=operand1
+    }
 }
 const operatorClicked=function(event){
     opTemp=event.target.textContent
-    
+    opFlag=true
     if(opTemp==='AC'){
-        operand1=undefined,operand2=undefined,operator=undefined;
+        operand1='',operand2='',operator=undefined;
         opTemp=undefined,ansTemp=undefined
-        flag=false;
+        eqFlag=false;
         display.innerHTML='&nbsp;'
+        opFlag=false
     }
 
     else if(opTemp==='='){
         // console.log(operand1,operand2,operator);
-        // console.log(operation(operand1,operand2,operator));
-        ansTemp=operation(operand1,operand2,operator)
+        // console.log(operate(operand1,operand2,operator));
+        ansTemp=operate(operand1,operand2,operator)
         display.textContent=ansTemp
-        operand1=undefined,operand2=undefined,operator=undefined;
-        flag=true
+        operand1="",operand2="",operator=undefined;
+        eqFlag=true
     }
     else{
-        if(flag)
+        if(eqFlag)
             operand1=ansTemp;
         display.textContent=opTemp
         operator=opTemp
     }
-    // console.log(flag);
+    // console.log(eqFlag);
     
 }
 
