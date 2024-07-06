86449e6631bf8c0284074e58329a08a40977d752
ctrlKshav <keshavm9978@gmail.com>
2024-07-06 13:31:04 +0530

    Linked the files in index and created a basic calculator logic in js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-﻿
+﻿function addTwoNumbers(op1,op2){
+    return op1+op2;
+}
+function subtractTwoNumbers(op1,op2){
+    return op1-op2;
+}
+function multiplyTwoNumbers(op1,op2){
+    return op1*op2;
+}
+function divideTwoNumbers(op1,op2){
+    return op1/op2;
+}
+
+let operand1,operand2,operator;
+function operation(op1,op2,opr){
+    if(opr==='+')
+        addTwoNumbers(op1,op2)
+    else if(opr==='-')
+        subtractTwoNumbers(op1,op2)
+    else if(opr==='*')
+        multiplyTwoNumbers(op1,op2)
+    else if(opr==='/')
+        divideTwoNumbers(op1,op2)
+    else
+        return 'invalid'
+}
+
